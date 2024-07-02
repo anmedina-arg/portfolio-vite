2e329d3f861ff338b650a10d8d5682c14a89a3c9
anmedina-arg <andres.medina.arg@gmail.com>
2024-07-02 09:53:36 -0300

    Cleaning CSS code in various sections offers a better approach, but it can be improved a bit more

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -1,31 +1,34 @@
 import "./experience.css";
 import SkillCard from "../../components/cards/SkillCard";
+import { Section } from "../../components/layout/section";
 
-import {FRONTEND_SKIL, BACKEND_SKILL} from '../../mockData/skills'
+import { FRONTEND_SKIL, BACKEND_SKILL } from '../../mockData/skills'
 
 function Experience() {
   return (
     <section id="experience">
-      <h5>What Skills I have</h5>
-      <h2>My experience</h2>
-      <div className="container experience__container">
-        <div className="experience__frontend">
-          <h3>Frontend development</h3>
-          <div className="experience__container">
-            {FRONTEND_SKIL.map((skill) => {
-              return <SkillCard skill={skill} />;
-            })}
+      <Section>
+        <h5>What Skills I have</h5>
+        <h2>My experience</h2>
+        <div className="experience__container">
+          <div className="experience__frontend">
+            <h3>Frontend development</h3>
+            <div className="experience__container">
+              {FRONTEND_SKIL.map((skill) => {
+                return <SkillCard skill={skill} />;
+              })}
+            </div>
+          </div>
+          <div className="experience__backend">
+            <h3>Backend development</h3>
+            <div className="experience__container">
+              {BACKEND_SKILL.map((skill) => {
+                return <SkillCard skill={skill} />;
+              })}
+            </div>
           </div>
         </div>
-        <div className="experience__backend">
-          <h3>Backend development</h3>
-          <div className="experience__container">
-            {BACKEND_SKILL.map((skill) => {
-              return <SkillCard skill={skill} />;
-            })}
-          </div>
-        </div>
-      </div>
+      </Section>
     </section>
   );
 }
--- src/sections/recomendations/Recomendations.tsx
+++ src/sections/recomendations/Recomendations.tsx
@@ -5,37 +5,40 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import 'swiper/css';
+import { Section } from "../../components/layout/section";
 
 import { reviews } from '../../mockData/recomendations';
 
 function Recomendations() {
   return (
     <section id="recomendations">
-      <h5>Some references</h5>
-      <h2>Recomendations</h2>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={50}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]} // Corrección aquí
-        className="container recomendations__container"
-      >
-        {reviews.map(({ id, avatar, name, review }) => {
-          return (
-            <SwiperSlide key={id} className="testimonial">
-              <div className="client__avatar">
-                <img src={avatar} alt="" />
-              </div>
-              <h5 className="client__name">{name}</h5>
-              <small className="client__review">{review}</small>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+      <Section>
+        <h5>Some references</h5>
+        <h2>Recomendations</h2>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={50}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]} // Corrección aquí
+          className="recomendations__container"
+        >
+          {reviews.map(({ id, avatar, name, review }) => {
+            return (
+              <SwiperSlide key={id} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} alt="" />
+                </div>
+                <h5 className="client__name">{name}</h5>
+                <small className="client__review">{review}</small>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </Section>
     </section>
   );
 }
